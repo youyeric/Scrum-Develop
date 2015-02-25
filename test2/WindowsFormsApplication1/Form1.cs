00adfe2abfdd8376c5e68e8f0788f20b6c214ade
youyeric <youyeric@gmail.com>
2015-02-25 23:12:10 +0800

    test

---
+++ test2/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        PicMatrix picmatrix = new PicMatrix(50,50);
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Size = new Size(1024, 768);
+            button1.Location = new Point(250,500);
+            button1.Text = "play";
+            picmatrix.picshow(this);
+            picmatrix.RandomShow();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            picmatrix.RandomShow();
+        }
+    }
+}
---
+++ test2/WindowsFormsApplication1/PicMatrix.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+using System.IO;
+
+namespace WindowsFormsApplication1
+{
+    class PicMatrix
+    {
+        private  int height;
+        private int width;
+        Random r = new Random();
+        PictureBox[,] picturearea = new PictureBox[4,4];
+       
+        public PicMatrix(int height,int width)
+        {
+            this.height = height;
+            this.width = width;
+        }
+        public void picshow(Form form1)
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                        picturearea[j,i]=new PictureBox();
+                        picturearea[j, i].Image = new Bitmap(@"C:\Users\Public\Pictures\Sample Pictures\Chrysanthemum.jpg");
+                        picturearea[j,i].Size=new Size(height,width);
+                        picturearea[j, i].Location = new Point(200+height*j,200+width*i);
+                        form1.Controls.Add(picturearea[j,i]);
+                        picturearea[j, i].Visible = false;
+                }
+            }
+        }
+        public void RandomShow()
+        {
+            int rs;
+            for (int i = 0; i < 4; i++)
+            {
+                rs=r.Next(0,4);
+                for (int j = 0; j < 4; j++)
+                {
+                    if (j==rs)
+                    {
+                        picturearea[j, i].Visible = true;
+                    }
+                    else
+                    {
+                        picturearea[j, i].Visible = false;
+                    }
+                }
+            }
+        }
+        public void PictureDown()
+        {
+ 
+        }
+    }
+}
